832472c983294a1d33563646fedeb985df116be7
Roman <romanpodgornykh@gmail.com>
2019-01-27 09:13:13 +0200

    Initial commit

---
+++ src/Cat.java
@@ -0,0 +1,15 @@
+public class Cat {
+    public static void main(String[] args) {
+
+
+        int a;
+        int b;
+        int c;
+        int d;
+        a = 3;
+        b = 1;
+        c = 2;
+        d = a + b - c;
+        System.out.println(d);
+    }
+}
